9f943298f94b50e3e0b5d57d08d9e820a3f87020
epeshk <peshkov@skbkontur.ru>
2019-01-15 16:52:14 +0500

    tag names in camelCase

--- Vostok.Logging.Hercules/Constants/ExceptionTagNames.cs
+++ Vostok.Logging.Hercules/Constants/ExceptionTagNames.cs
@@ -2,9 +2,9 @@ namespace Vostok.Logging.Hercules.Constants
 {
     internal static class ExceptionTagNames
     {
-        public const string InnerExceptions = nameof(InnerExceptions);
-        public const string StackTrace = nameof(StackTrace);
-        public const string Message = nameof(Message);
-        public const string Type = nameof(Type);
+        public const string InnerExceptions = "innerExceptions";
+        public const string StackTrace = "stackTrace";
+        public const string Message = "message";
+        public const string Type = "type";
     }
 }
--- Vostok.Logging.Hercules/Constants/LogEventTagNames.cs
+++ Vostok.Logging.Hercules/Constants/LogEventTagNames.cs
@@ -2,11 +2,10 @@ namespace Vostok.Logging.Hercules.Constants
 {
     internal static class LogEventTagNames
     {
-        public const string UtcOffset = nameof(UtcOffset);
-        public const string Exception = nameof(Exception);
-        public const string MessageTemplate = nameof(MessageTemplate);
-        public const string RenderedMessage = nameof(RenderedMessage);
-        public const string Properties = nameof(Properties);
-        public const string AdditionalFields = nameof(AdditionalFields);
+        public const string UtcOffset = "utcOffset";
+        public const string Exception = "exception";
+        public const string MessageTemplate = "messageTemplate";
+        public const string RenderedMessage = "renderedMessage";
+        public const string Properties = "properties";
     }
 }
