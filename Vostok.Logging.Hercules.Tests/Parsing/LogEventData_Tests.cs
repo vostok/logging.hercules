c5682efca2383bdc310fed430085634670f2a42f
epeshk <peshkov@skbkontur.ru>
2019-01-16 17:23:22 +0500

    add tests for LogEventData

---
+++ Vostok.Logging.Hercules.Tests/Parsing/LogEventData_Tests.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Runtime.InteropServices;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using NUnit.Framework;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Logging.Hercules.Constants;
+using Vostok.Logging.Hercules.Parsing;
+
+namespace Vostok.Logging.Hercules.Tests.Parsing
+{
+    public class LogEventData_Tests
+    {
+        [Test]
+        public void Should_not_fail_on_empty_HerculesEvent()
+        {
+            var @event = new HerculesEvent(DateTimeOffset.Now, HerculesTags.Empty);
+
+            var logEventData = new LogEventData(@event);
+
+            logEventData.Exception.Should().BeNull();
+            logEventData.Properties.Should().BeNull();
+            logEventData.MessageTemplate.Should().BeNull();
+            logEventData.RenderedMessage.Should().BeNull();
+        }
+
+        [Test]
+        public void Should_extract_values()
+        {
+            var offset = 10.Hours();
+            var timestamp = DateTimeOffset.Now.ToOffset(offset);
+            var messageTemplate = Guid.NewGuid().ToString();
+            var renderedMessage = Guid.NewGuid().ToString();
+            
+            var eventBuilder = new HerculesEventBuilder();
+            
+            eventBuilder
+                .SetTimestamp(timestamp)
+                .AddValue(LogEventTagNames.MessageTemplate, messageTemplate)
+                .AddValue(LogEventTagNames.RenderedMessage, renderedMessage)
+                .AddContainer(LogEventTagNames.Properties, delegate { })
+                .AddContainer(LogEventTagNames.Exception, delegate { });
+
+            var @event = eventBuilder.BuildEvent();
+
+            var logEventData = new LogEventData(@event);
+
+            logEventData.Timestamp.Should().Be(timestamp);
+            logEventData.MessageTemplate.Should().Be(messageTemplate);
+            logEventData.RenderedMessage.Should().Be(renderedMessage);
+            logEventData.Properties.Should().NotBeNull();
+            logEventData.Exception.Should().NotBeNull();
+        }
+    }
+}
