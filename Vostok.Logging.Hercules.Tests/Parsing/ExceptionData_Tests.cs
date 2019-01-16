4896e927323be3b8c5489fb75953e242956000f3
epeshk <peshkov@skbkontur.ru>
2019-01-16 17:43:56 +0500

    add tests for StackFrameData

---
+++ Vostok.Logging.Hercules.Tests/Parsing/ExceptionData_Tests.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Runtime.InteropServices;
+using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Logging.Hercules.Constants;
+using Vostok.Logging.Hercules.Parsing;
+
+namespace Vostok.Logging.Hercules.Tests.Parsing
+{
+    public class ExceptionData_Tests
+    {
+        [Test]
+        public void Should_not_fail_on_empty_tags()
+        {
+            var logEventData = ExceptionData.FromTags(HerculesTags.Empty);
+
+            logEventData.Type.Should().BeNull();
+            logEventData.Message.Should().BeNull();
+            logEventData.StackTrace.Should().BeNull();
+            logEventData.InnerExceptions.Should().BeNull();
+        }
+
+        [Test]
+        public void Should_extract_values()
+        {
+            var type = Guid.NewGuid().ToString();
+            var message = Guid.NewGuid().ToString();
+            
+            var tagsBuilder = new HerculesTagsBuilder();
+            
+            tagsBuilder
+                .AddValue(ExceptionTagNames.Type, type)
+                .AddValue(ExceptionTagNames.Message, message)
+                .AddVectorOfContainers(ExceptionTagNames.StackTrace, new Action<IHerculesTagsBuilder>[0])
+                .AddVectorOfContainers(ExceptionTagNames.InnerExceptions, new Action<IHerculesTagsBuilder>[0]);
+
+            var tags = tagsBuilder.BuildTags();
+
+            var exceptionData = ExceptionData.FromTags(tags);
+
+            exceptionData.Type.Should().Be(type);
+            exceptionData.Message.Should().Be(message);
+            exceptionData.StackTrace.Should().NotBeNull();
+            exceptionData.InnerExceptions.Should().NotBeNull();
+        }
+    }
+}
---
+++ Vostok.Logging.Hercules.Tests/Parsing/StackFrameData_Tests.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Runtime.InteropServices;
+using FluentAssertions;
+using FluentAssertions.Extensions;
+using NUnit.Framework;
+using Vostok.Hercules.Client.Abstractions.Events;
+using Vostok.Logging.Hercules.Constants;
+using Vostok.Logging.Hercules.Parsing;
+
+namespace Vostok.Logging.Hercules.Tests.Parsing
+{
+    public class StackFrameData_Tests
+    {
+        [Test]
+        public void Should_not_fail_on_empty_tags()
+        {
+            var stackFrameData = new StackFrameData(HerculesTags.Empty);
+
+            stackFrameData.Type.Should().BeNull();
+            stackFrameData.Function.Should().BeNull();
+            stackFrameData.File.Should().BeNull();
+            stackFrameData.Line.Should().BeNull();
+            stackFrameData.Column.Should().BeNull();
+        }
+
+        [Test]
+        public void Should_extract_values()
+        {
+            var type = Guid.NewGuid().ToString();
+            var function = Guid.NewGuid().ToString();
+            var file = Guid.NewGuid().ToString();
+            var line = 25;
+            var column = 80;
+            
+            var tagsBuilder = new HerculesTagsBuilder();
+
+            tagsBuilder
+                .AddValue(StackFrameTagNames.Type, type)
+                .AddValue(StackFrameTagNames.Function, function)
+                .AddValue(StackFrameTagNames.File, file)
+                .AddValue(StackFrameTagNames.Line, line)
+                .AddValue(StackFrameTagNames.Column, column);
+
+            var tags = tagsBuilder.BuildTags();
+
+            var stackFrameData = new StackFrameData(tags);
+
+            stackFrameData.Type.Should().Be(type);
+            stackFrameData.Function.Should().Be(function);
+            stackFrameData.File.Should().Be(file);
+            stackFrameData.Line.Should().Be(line);
+            stackFrameData.Column.Should().Be(column);
+        }
+    }
+}
