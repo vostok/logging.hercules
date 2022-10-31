3052aa6db6f3d1a37ce7a06f9b7b67a9cec94a6c
a.tolstov <a.tolstov@skbkontur.ru>
2022-10-31 11:48:58 +0500

    fix(LogEventData): Timestamp parsing

--- Vostok.Logging.Hercules.Tests/Parsing/LogEventData_Tests.cs
+++ Vostok.Logging.Hercules.Tests/Parsing/LogEventData_Tests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using FluentAssertions;
 using FluentAssertions.Extensions;
 using NUnit.Framework;
@@ -24,6 +25,35 @@ namespace Vostok.Logging.Hercules.Tests.Parsing
             logEventData.Message.Should().BeNull();
         }
 
+        [Test]
+        public void Should_parse_date()
+        {
+            var stackTrace = Guid.NewGuid().ToString();
+            var messageTemplate = Guid.NewGuid().ToString();
+            var renderedMessage = Guid.NewGuid().ToString();
+            var propKey = "prop1";
+            var propValue = 2;
+
+            var eventBuilder = new HerculesEventBuilder();
+
+            eventBuilder
+                .SetTimestamp(DateTimeOffset.ParseExact("10/31/2022 6:36:14 AM +00:00", "MM/dd/yyyy h:mm:ss tt zzz", CultureInfo.InvariantCulture.DateTimeFormat))
+                .AddValue(LogEventTagNames.UtcOffset, 108000000000L)
+                .AddValue(LogEventTagNames.MessageTemplate, messageTemplate)
+                .AddValue(LogEventTagNames.Message, renderedMessage)
+                .AddValue(LogEventTagNames.StackTrace, stackTrace)
+                .AddContainer(LogEventTagNames.Properties, b => b.AddValue(propKey, propValue))
+                .AddContainer(LogEventTagNames.Exception, delegate {});
+
+            var @event = eventBuilder.BuildEvent();
+
+            var logEventData = new LogEventData(@event);
+
+            TestContext.Out.WriteLine(logEventData.Timestamp);
+            
+            logEventData.Timestamp.UtcDateTime.Should().Be(DateTime.ParseExact("10/31/2022 6:36:14 AM", "MM/dd/yyyy h:mm:ss tt", CultureInfo.InvariantCulture.DateTimeFormat));
+        }
+        
         [Test]
         public void Should_extract_values()
         {
